4615f10292bfb008005ca537dc3d0e10fdd809da
Giriprasad <giriprasad.vasudevan@gmail.com>
2019-11-07 15:16:39 +0000

    first commit

---
+++ my-app/src/components/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import logo from '../styles/logo.svg';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          A place to post your concerns...
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Welcome To Voice of Tamil Nadu
+        </a>
+
+        <body>
+          <a
+            className="App-left-sidebar"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Main Menu
+        </a>
+        </body>
+      </header>
+    </div>
+  );
+}
+
+export default App;
